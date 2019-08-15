7e04f6e26fc08294214282975aa7e16d3f81b07b
1219858677@qq.com <1219858677@qq.com>
2019-08-15 17:38:11 +0800

    next.js

---
+++ hello-next/pages/about.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+
+export default function About() {
+    return (
+        <div>
+            <Header/>
+            <p>This is a about page</p>
+        </div>
+    )
+}
---
+++ hello-next/pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+
+const Index = () => (
+    <div>
+        <Header/>
+        <p>hello Next.js</p>
+    </div>
+);
+
+export default Index
